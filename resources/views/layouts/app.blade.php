84d4b8fe64176cad6f986b32446ea65418430940
imran_codalo <imran.codalo@gmail.com>
2017-12-26 16:43:00 +0600

    post form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,29 @@
             </div>
         </nav>
 
-        @yield('content')
+
+        <div class="container">
+            <div class="row">
+
+                @if(Auth::check())
+                <div class="col-md-4">
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <a href="/home">Home</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="{{ route('post.create') }}">Create New Post</a>
+                        </li>
+                    </ul>
+                </div>
+                @endif
+
+                <div class="col-md-8">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+
     </div>
 
     <!-- Scripts -->
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -19,4 +8,9 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('post', 'PostsController');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+
+    Route::resource('post', 'PostsController');
+
+});
+
