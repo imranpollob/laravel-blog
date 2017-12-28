c064c5de59a9468cfdf945db389cfa5eb3f634e7
imran_codalo <imran.codalo@gmail.com>
2017-12-28 17:24:35 +0600

    SoftDeletes at Post

--- app/Post.php
+++ app/Post.php
@@ -3,11 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
 
     protected $fillable = ['title', 'category_id', 'featured', 'content'];
+
+    protected $dates = ['deleted_at'];
 
     public function category()
     {
